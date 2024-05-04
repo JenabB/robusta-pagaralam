6fab99c07cb52bbcc5dc69d0d9a98482af2dec03
JenabB <birulangit199^@gmail.com>
2024-05-04 16:45:58 +0700

    update flagship

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
             <ul className='flex space-x-12'>
                 {navbar.map((item, index) => (
                     <li key={index}>
-                        <a className='font-caveat font-bold text-2xl' href={`${item.path}`}>
+                        <a className='font-poppins font-bold text-lg' href={`${item.path}`}>
                             {item.name}
                         </a>
                     </li>
--- app/components/ProductHighlight.tsx
+++ app/components/ProductHighlight.tsx
@@ -17,62 +17,58 @@ const ProductHighlight = () => {
         <div>
             <div>
                 <hr className="mb-4" />
-                {tea.tea.map((item, index) => (
-                    <div key={index}>
-                        <div>
-                            <div className="mb-10">
-                                <div className="">
-                                    <div className="p-8 pt-0">
-                                        <h1 className="font-bold text-xl uppercase font-sans">
-                                            {item.name}
-                                        </h1>
-                                        <p className="mt-4 font-edu text-xl">{item.description}</p>
-                                    </div>
+                <div className='lg:grid lg:grid-cols-2'>
+                    {tea.tea.map((item, index) => (
+                        <div key={index}>
+                            <div className="p-4 mb-10">
+                                <div className=" pt-0">
+                                    <h1 className="font-bold text-xl uppercase font-sans">
+                                        {item.name}
+                                    </h1>
+                                    <p className="mt-4 font-edu text-lg">{item.description}</p>
                                 </div>
-                                <div>
-                                    <div className="space-x-4 text-center flex overflow-x-scroll px-4 mt-8">
-                                        {item.child.map((x, i) => (
-                                            <div key={i}>
-                                                <div
-                                                    className="shadow-xl rounded-b-md"
-                                                    style={{ width: '250px' }}
-                                                >
-                                                    <img
-                                                        className="rounded-t-lg"
-                                                        src={x.image}
-                                                        style={{
-                                                            height: '200px',
-                                                            width: '250px',
-                                                            objectFit: 'cover',
-                                                        }}
-                                                    />
-                                                    <div className="px-2 pb-2 bg-slate-100">
-                                                        <div className="flex justify-between items-center">
-                                                            <p className="p-2 font-poppins font-bold text-sm">
-                                                                {x.name}
-                                                            </p>
-                                                            <p className="text-orange-500 font-semibold text-sm">
-                                                                {x.price}
-                                                            </p>
-                                                        </div>
-                                                        <div className="text-left mt-2">
-                                                            <button
-                                                                className="px-4 py-2 bg-black text-white rounded-2xl font-bold text-xs"
-                                                                onClick={() => handleBuyNowClick(x.name)}
-                                                            >
-                                                                SHOP NOW
-                                                            </button>
-                                                        </div>
+                                <div className="space-x-4 text-center flex overflow-x-scroll px-4 mt-8">
+                                    {item.child.map((x, i) => (
+                                        <div key={i}>
+                                            <div
+                                                className="shadow-xl rounded-b-md"
+                                                style={{ width: '250px' }}
+                                            >
+                                                <img
+                                                    className="rounded-t-lg"
+                                                    src={x.image}
+                                                    style={{
+                                                        height: '200px',
+                                                        width: '250px',
+                                                        objectFit: 'cover',
+                                                    }}
+                                                />
+                                                <div className="px-2 pb-2 bg-slate-100">
+                                                    <div className="flex justify-between items-center">
+                                                        <p className="p-2 font-poppins font-bold text-sm">
+                                                            {x.name}
+                                                        </p>
+                                                        <p className="text-orange-500 font-semibold text-sm">
+                                                            {x.price}
+                                                        </p>
+                                                    </div>
+                                                    <div className="text-left mt-2">
+                                                        <button
+                                                            className="px-4 py-2 bg-black text-white rounded-2xl font-bold text-xs"
+                                                            onClick={() => handleBuyNowClick(x.name)}
+                                                        >
+                                                            SHOP NOW
+                                                        </button>
                                                     </div>
                                                 </div>
                                             </div>
-                                        ))}
-                                    </div>
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
